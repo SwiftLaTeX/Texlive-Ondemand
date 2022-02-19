159171463dec8066478e7369fff6f004b1ed8090
Elliott Wen <jq.elliott.wen@gmail.com>
2022-02-20 10:47:12 +1300

    remove fontconfig

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import time
 import os.path
 import pykpathsea_xetex
 import pykpathsea_pdftex
-import pyfontconfig
 from flask_cors import cross_origin
 import re
 import os
@@ -17,84 +16,85 @@ def san(name):
     return regex.sub('', name)
 
 
-fontconfig_lock = Lock()
-font_hit_db = {}
-@app.route('/fontconfig/<fontvar>/<fontname>')
+
+@app.route('/fontconfig/<path:fontname>')
 @cross_origin()
-def fetch_font(fontvar, fontname):
-    cacheKey = fontvar + fontname
+def fetch_font(fontname):
+    ext_ok = False
+    allowed_exts = [".otf", ".ttf", ".woff", ".t1", ".pfb"]
+    for ext in allowed_exts:
+        if fontname.endswith(ext):
+            ext_ok = True
+            break
 
-    if not cacheKey in font_hit_db:
-        requiredItalic = 0
-        if 'I' in fontvar:
-            requiredItalic = 1
+    if not ext_ok:
+        return "File not found", 301
 
-        requiredBold = 0
-        if 'B' in fontvar:
-            requiredBold = 1
-        
-        with fontconfig_lock:
-            res = pyfontconfig.find_font(san(fontname), requiredBold, requiredItalic)
+    url = "/usr/share/" + fontname
 
-        if res is None or not os.path.isfile(res) or not (res.endswith('.ttf') or res.endswith('.otf')):
-            return "Font not found", 301
-        else:
-            font_hit_db[cacheKey] = res
+    if not os.path.isfile(url):
+        return "File not found", 301
     
-    url = font_hit_db[cacheKey]
-    response = make_response(send_file(url, mimetype='font/otf'))
+    response = make_response(send_file(url, mimetype='application/octet-stream'))
     response.headers['fontid'] = os.path.basename(url)
     response.headers['Access-Control-Expose-Headers'] = 'fontid'
     return response
 
 
-kpathsea_xetex_lock = Lock()
-xetex_file_hit_db = {}
-
-@app.route('/xetex/<filename>')
+@app.route('/xetex/<int:fileformat>/<filename>')
 @cross_origin()
-def xetex_fetch_file(filename):
+def xetex_fetch_file(fileformat, filename):
     
+    url = None
     if filename == "swiftlatexxetex.fmt":
-        return send_file(filename, mimetype="application/octet-stream")
+        url = filename
+    else:
+        url = pykpathsea_xetex.find_file(san(filename), fileformat)
 
-    if not filename in xetex_file_hit_db:
-        with kpathsea_xetex_lock:
-            res = pykpathsea_xetex.find_file(san(filename))
-
-        if res is None or not os.path.isfile(res):
-            return "File not found", 301
-        else:
-            xetex_file_hit_db[filename] = res
-    
-    urls = xetex_file_hit_db[filename]
-    return send_file(urls, mimetype="application/octet-stream")
+    if url is None or not os.path.isfile(url):
+        return "File not found", 301
+    else:
+        response = make_response(send_file(url, mimetype='application/octet-stream'))
+        response.headers['fileid'] = os.path.basename(url)
+        response.headers['Access-Control-Expose-Headers'] = 'fileid'
+        return response
 
 
-kpathsea_pdftex_lock = Lock()
-pdftex_file_hit_db = {}
 
-@app.route('/pdftex/<filename>')
+@app.route('/pdftex/<int:fileformat>/<filename>')
 @cross_origin()
-def pdftex_fetch_file(filename):
+def pdftex_fetch_file(fileformat, filename):
     
+    url = None
     if filename == "swiftlatexpdftex.fmt":
-        return send_file(filename, mimetype="application/octet-stream")
+        url = filename
+    else:
+        url = pykpathsea_pdftex.find_file(san(filename), fileformat)
 
-    if not filename in pdftex_file_hit_db:
-        with kpathsea_pdftex_lock:
-            res = pykpathsea_pdftex.find_file(san(filename))
+    if url is None or not os.path.isfile(url):
+        return "File not found", 301
+    else:
+        response = make_response(send_file(url, mimetype='application/octet-stream'))
+        response.headers['fileid'] = os.path.basename(url)
+        response.headers['Access-Control-Expose-Headers'] = 'fileid'
+        return response
+            
 
-        if res is None or not os.path.isfile(res):
+
+@app.route('/pdftex/pk/<int:dpi>/<filename>')
+@cross_origin()
+def pdftex_fetch_pk(dpi, filename):
+    
+    with kpathsea_pdftex_lock:
+        url = pykpathsea_pdftex.find_pk(san(filename), dpi)
+
+        if url is None or not os.path.isfile(url):
             return "File not found", 301
         else:
-            pdftex_file_hit_db[filename] = res
-    
-    urls = pdftex_file_hit_db[filename]
-    return send_file(urls, mimetype="application/octet-stream")
-
-
-
+            response = make_response(send_file(url, mimetype='application/octet-stream'))
+            response.headers['pkid'] = os.path.basename(url)
+            response.headers['Access-Control-Expose-Headers'] = 'pkid'
+            return response
 
 
 
--- pykpathsea_pdftex.c
+++ pykpathsea_pdftex.c
@@ -33,225 +33,31 @@
 #include <string.h>
 
 static kpathsea kpse = NULL;
+static unsigned int DPI = 600;
 
-/* Base resolution. (-D, -dpi) */
-static unsigned dpi = 600;
-
-/* Search the disk as well as ls-R?  (-must-exist, -mktex) */
-static boolean must_exist = false;
-
-/* The file type and path for lookups.  (-format, -path) */
-
-/* Define one-word abbreviations for those format types which
-   can otherwise only be specified by strings containing spaces.  */
-
-typedef struct {
-  const_string abbr;
-  kpse_file_format_type format;
-} format_abbr_type;
-
-static format_abbr_type format_abbrs[] = {
-    {"bitmapfont", kpse_any_glyph_format},
-    {"mpsupport", kpse_mpsupport_format},
-    {"doc", kpse_texdoc_format},
-    {"source", kpse_texsource_format},
-    {"trofffont", kpse_troff_font_format},
-    {"dvipsconfig", kpse_dvips_config_format},
-    {"web2c", kpse_web2c_format},
-    {"othertext", kpse_program_text_format},
-    {"otherbin", kpse_program_binary_format},
-    {"miscfont", kpse_miscfonts_format},
-    {"cmap", kpse_cmap_format},
-    {"pdftexconfig", kpse_pdftex_config_format},
-    {NULL, kpse_last_format}};
-
-/* The function to look up STR in the abbr table above.
-   This is called only on a user-specified format string.
-   Return `kpse_last_format' if no match.  */
-
-static kpse_file_format_type format_abbr(const_string str) {
-  kpse_file_format_type ret = kpse_last_format;
-  unsigned a = 0;
-
-  while (format_abbrs[a].abbr != NULL) {
-    if (STREQ(str, format_abbrs[a].abbr)) {
-      ret = format_abbrs[a].format;
-      break;
-    }
-    a++;
+static PyObject *py_kpse_find_pk(PyObject *self, PyObject *args) {
+  char *filename;
+  int dpi;
+  char *completefilename;
+  PyObject *returnvalue;
+  if (PyArg_ParseTuple(args, "si", &filename, &dpi)) {
+    kpse_glyph_file_type font_file_ex;
+    completefilename = kpathsea_find_glyph(kpse, filename, dpi, kpse_pk_format, &font_file_ex);
+    returnvalue = Py_BuildValue("s", completefilename);
+    if (completefilename != NULL)
+      free(completefilename);
+    return returnvalue;
   }
-
-  return ret;
-}
-
-/* Return the <number> substring in `<name>.<number><stuff>', if S has
-   that form.  If it doesn't, return 0.  */
-
-static unsigned find_dpi(string s) {
-  unsigned dpi_number = 0;
-  const_string extension = find_suffix(s);
-
-  if (extension != NULL)
-    sscanf(extension, "%u", &dpi_number);
-
-  return dpi_number;
-}
-
-/* Return true if FTRY (the candidate suffix) matches NAME.  If
-   IS_FILENAME is true, the check is simply that FTRY is a suffix of
-   NAME.  If false (that is, NAME is a format), then FTRY and NAME must
-   be entirely equal.  */
-
-static boolean try_suffix(boolean is_filename, string name, unsigned name_len,
-                          const_string ftry) {
-  unsigned try_len;
-
-  if (!ftry || !*ftry) {
-    return false;
-  }
-
-  try_len = strlen(ftry);
-  if (try_len > name_len) {
-    /* Candidate is longer than what we're looking for.  */
-    return false;
-  }
-  if (!is_filename && try_len < name_len) {
-    /* We're doing format names, not file names, and candidate is
-       shorter than what we're looking for.  E.g., do not find `lua'
-       when looking for `clua'.  */
-    return false;
-  }
-
-  if (FILESTRCASEEQ(name + name_len - try_len, ftry)) {
-    return true;
-  }
-
-  return false;
-}
-
-/* Use the file type from -format if that was previously determined
-   (i.e., the user_format global variable), else guess dynamically from
-   NAME.  Return kpse_last_format if undeterminable.  This function is
-   also used to parse the -format string, a case we distinguish via
-   is_filename being false.
-
-   A few filenames have been hard-coded for format types that
-   differ from what would be inferred from their extensions. */
-
-static kpse_file_format_type find_format(kpathsea kpse, string name,
-                                         boolean is_filename) {
-  kpse_file_format_type ret = kpse_last_format;
-
-  if (FILESTRCASEEQ(name, "config.ps")) {
-    ret = kpse_dvips_config_format;
-  } else if (FILESTRCASEEQ(name, "fmtutil.cnf")) {
-    ret = kpse_web2c_format;
-  } else if (FILESTRCASEEQ(name, "glyphlist.txt")) {
-    ret = kpse_fontmap_format;
-  } else if (FILESTRCASEEQ(name, "mktex.cnf")) {
-    ret = kpse_web2c_format;
-  } else if (FILESTRCASEEQ(name, "pdfglyphlist.txt")) {
-    ret = kpse_fontmap_format;
-  } else if (FILESTRCASEEQ(name, "pdftex.cfg")) {
-    ret = kpse_pdftex_config_format;
-  } else if (FILESTRCASEEQ(name, "texglyphlist.txt")) {
-    ret = kpse_fontmap_format;
-  } else if (FILESTRCASEEQ(name, "texmf.cnf")) {
-    ret = kpse_cnf_format;
-  } else if (FILESTRCASEEQ(name, "updmap.cfg")) {
-    ret = kpse_web2c_format;
-  } else if (FILESTRCASEEQ(name, "XDvi")) {
-    ret = kpse_program_text_format;
-  } else {
-    if (!is_filename) {
-      /* Look for kpsewhich-specific format abbreviations.  */
-      ret = format_abbr(name);
-    }
-
-    if (ret == kpse_last_format) {
-      int f = 0; /* kpse_file_format_type */
-      unsigned name_len = strlen(name);
-
-      while (f != kpse_last_format) {
-        const_string *ext;
-        const_string ftry;
-        boolean found = false;
-
-        if (!kpse->format_info[f].type)
-          kpathsea_init_format(kpse, (kpse_file_format_type)f);
-
-/* Just to abbreviate this lengthy call.  */
-#define TRY_SUFFIX(ftry) try_suffix(is_filename, name, name_len, (ftry))
-
-        if (!is_filename) {
-          /* Allow the long name, but only in the format options.  We don't
-             want a filename confused with a format name.  */
-          ftry = kpse->format_info[f].type;
-          found = TRY_SUFFIX(ftry);
-        }
-        for (ext = kpse->format_info[f].suffix; !found && ext && *ext; ext++) {
-          found = TRY_SUFFIX(*ext);
-          // printf("Trying1 %s %d\n", *ext, found);
-        }
-        for (ext = kpse->format_info[f].alt_suffix; !found && ext && *ext;
-             ext++) {
-          found = TRY_SUFFIX(*ext);
-          // printf("Trying2 %s %d\n", *ext, found);
-        }
-
-        if (found)
-          break;
-        f++;
-      }
-      ret = f;
-    }
-  }
-  return ret;
-}
-
-/* Look up a single filename NAME.  Return 0 if success, 1 if failure.  */
-
-static string lookup(kpathsea kpse, string name) {
-
-  string ret = NULL;
-
-  /* No user-specified search path, check user format or guess from NAME.  */
-  kpse_file_format_type fmt = find_format(kpse, name, true);
-
-  switch (fmt) {
-  case kpse_pk_format:
-  case kpse_gf_format:
-  case kpse_any_glyph_format: {
-    kpse_glyph_file_type glyph_ret;
-    string temp = remove_suffix(name);
-
-    /* Try to extract the resolution from the name.  */
-    unsigned local_dpi = find_dpi(name);
-    if (!local_dpi)
-      local_dpi = dpi;
-    ret = kpathsea_find_glyph(kpse, temp, local_dpi, fmt, &glyph_ret);
-    if (temp != name)
-      free(temp);
-  } break;
-
-  case kpse_last_format:
-    /* If the suffix isn't recognized, assume it's a tex file. */
-    fmt = kpse_tex_format;
-    /* fall through */
-
-  default:
-    ret = kpathsea_find_file(kpse, name, fmt, must_exist);
-  }
-
-  return ret;
+  return NULL;
 }
 
 static PyObject *py_kpse_find_file(PyObject *self, PyObject *args) {
   char *filename;
+  int fmt;
   char *completefilename;
   PyObject *returnvalue;
-  if (PyArg_ParseTuple(args, "s", &filename)) {
-    completefilename = lookup(kpse, filename);
+  if (PyArg_ParseTuple(args, "si", &filename, &fmt)) {
+    completefilename = kpathsea_find_file(kpse, filename, fmt, 0);
     returnvalue = Py_BuildValue("s", completefilename);
     if (completefilename != NULL)
       free(completefilename);
@@ -264,6 +70,7 @@ static PyObject *py_kpse_find_file(PyObject *self, PyObject *args) {
 
 static PyMethodDef pykpathsea_pdftex_methods[] = {
     {"find_file", (PyCFunction)py_kpse_find_file, METH_VARARGS, NULL},
+    {"find_pk", (PyCFunction)py_kpse_find_pk, METH_VARARGS, NULL},
     {NULL, NULL}};
 
 static struct PyModuleDef moduledef = {PyModuleDef_HEAD_INIT,
@@ -293,7 +100,7 @@ PyMODINIT_FUNC PyInit_pykpathsea_pdftex(void) {
 
   kpathsea_xputenv(kpse, "engine", "pdftex");
 
-  kpathsea_init_prog(kpse, uppercasify(kpse->program_name), dpi, NULL, NULL);
+  kpathsea_init_prog(kpse, uppercasify(kpse->program_name), DPI, NULL, NULL);
 
   return module;
 }
